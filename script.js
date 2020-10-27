353db1e43f32043788b8bf2fd84a44fb15831496
Lucas Molenaar <lucas@MacBook-Pro-van-Lucas.local>
2020-10-27 11:08:05 +0100

    css change

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         document.body.style.backgroundColor = box.style.backgroundColor;
+        box.style.transition = 'all 0.3s ease-out';
     })
 })
 
 boxes.forEach(box => {
     box.addEventListener('mouseleave', () => {
         document.body.style.backgroundColor = 'transparent';
+        box.style.transition = 'all 0.3s ease-out';
     })
 })
